043385bbe83e6a60a1c5aeaeeece815370ac4144
vuongicesmile <boide03@gmail.com>
2021-02-19 03:48:44 +0700

    Phần 94-Tạo giao diện Web Shop -Frontend

---
+++ eshopper/resources/views/components/footer.blade.php
@@ -0,0 +1,13 @@
+<footer id="footer"><!--Footer-->
+
+    <div class="footer-bottom">
+        <div class="container">
+            <div class="row">
+                <p class="pull-left">Copyright © 2013 E-SHOPPER Inc. All rights reserved.</p>
+                <p class="pull-right">Designed by <span><a target="_blank"
+                                                           href="http://www.themeum.com">Themeum</a></span></p>
+            </div>
+        </div>
+    </div>
+
+</footer><!--/Footer-->
---
+++ eshopper/resources/views/layouts/master.blade.php
@@ -0,0 +1,37 @@
+<html>
+<head>
+
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    @yield('title')
+
+    <link href="{{asset('eshopper/css/bootstrap.min.css')}}" rel="stylesheet">
+    <link href="{{asset('eshopper/css/font-awesome.min.css')}}" rel="stylesheet">
+    <link href="{{asset('eshopper/css/prettyPhoto.css')}}" rel="stylesheet">
+    <link href="{{asset('eshopper/css/price-range.css')}}" rel="stylesheet">
+    <link href="{{asset('eshopper/css/animate.css')}}" rel="stylesheet">
+    <link href="{{asset('eshopper/css/main.css')}}" rel="stylesheet">
+    @yield('css')
+
+</head>
+<body>
+
+@include('components.header')
+@yield('content')
+@include('components.footer')
+
+
+<script src="{{asset('eshopper/js/jquery.js')}}"></script>
+<script src="{{asset('eshopper/js/bootstrap.min.js')}}"></script>
+<script src="{{asset('eshopper/js/jquery.scrollUp.min.js')}}"></script>
+<script src="{{asset('eshopper/js/price-range.js')}}"></script>
+<script src="{{asset('eshopper/js/jquery.prettyPhoto.js')}}"></script>
+<script src="{{asset('eshopper/js/main.js')}}"></script>
+
+@yield('js')
+
+</body>
+</html>
